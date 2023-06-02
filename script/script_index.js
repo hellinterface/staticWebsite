67091cc2cc5f94e3c9bad0894ec59ca32f5f78f1
pumpkin012 <srx.pumpkin@gmail.com>
2023-06-03 02:02:01 +0300

    Merge branch 'main' of https://github.com/hellinterface/staticWebsite

--- script/script_index.js
+++ script/script_index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(){
     var lastKnownScrollPosition = 0;
 
     window.addEventListener('scroll', (event) => {
-        if (mainContent.scrollY >= window.visualViewport.height) {
+        if (window.scrollY >= window.visualViewport.height) {
             setTimeout(() => {
                 document.body.classList.add('pastTitleScreen');
             }, 200);
@@ -12,8 +12,46 @@ document.addEventListener("DOMContentLoaded", function(){
             document.body.classList.remove('pastTitleScreen');
         }
         lastKnownScrollPosition = window.scrollY;
-        console.log(mainContent.scrollY, window.visualViewport.height);
     });
     
+    // Категория "События" правая и левая кнопки ленты
+    lenta2.currentElementIndex = 0;
+
+    ring3.classList.add('disabledRing');
+
+    ring4.addEventListener('click', () => {
+        if (lenta2.scrollWidth < lenta2.children[lenta2.currentElementIndex].offsetLeft + lenta2.offsetWidth) {
+            return;
+        }
+        lenta2.currentElementIndex++;
+        if (lenta2.currentElementIndex >= lenta2.children.length - 1) {
+            lenta2.currentElementIndex = lenta2.children.length - 1;
+            ring4.classList.add('disabledRing');
+        }
+        else {
+            ring4.classList.remove('disabledRing');
+            ring3.classList.remove('disabledRing');
+        }
+        console.log(lenta2.currentElementIndex);
+        lenta2.scrollTo({left: lenta2.children[lenta2.currentElementIndex].offsetLeft, behavior: 'smooth'});
+        if (lenta2.scrollWidth < lenta2.children[lenta2.currentElementIndex].offsetLeft + lenta2.offsetWidth) {
+            ring4.classList.add('disabledRing');
+        }
+    });
+    ring3.addEventListener('click', () => {
+        lenta2.currentElementIndex--;
+        if (lenta2.currentElementIndex == 0) {
+            ring3.classList.add('disabledRing');
+        }
+        else {
+            ring3.classList.remove('disabledRing');
+            ring4.classList.remove('disabledRing');
+        }
+        if (lenta2.currentElementIndex <= 0) {
+            lenta2.currentElementIndex = 0;
+        }
+        console.log(lenta2.currentElementIndex);
+        lenta2.scrollTo({left: lenta2.children[lenta2.currentElementIndex].offsetLeft, behavior: 'smooth'});
+    });
 
 });
--- script/script_main.js
+++ script/script_main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
+    
     // функции "открыть" и "закрыть" меню
     menuToggle.addEventListener('click', () => {
         document.body.classList.toggle('menuOpened');
@@ -9,43 +10,4 @@ document.addEventListener("DOMContentLoaded", function(){
     const mainContent = document.querySelector('.content');
 
 
-    // Категория "События" правая и левая кнопки ленты
-    var i = 0;
-    var Item2 = document.getElementsByClassName('item2');
-    var sumLenta = Item2.length * 576;
-    lenta2.currentElementIndex = 0;
-    ring4.addEventListener('click', () => {
-        if (lenta2.scrollWidth < lenta2.children[lenta2.currentElementIndex].offsetLeft + lenta2.offsetWidth) {
-            return;
-        }
-        lenta2.currentElementIndex++;
-        if (lenta2.currentElementIndex >= lenta2.children.length - 1) {
-            lenta2.currentElementIndex = lenta2.children.length - 1;
-            ring4.classList.add('disabledRing');
-        }
-        else {
-            ring4.classList.remove('disabledRing');
-            ring3.classList.remove('disabledRing');
-        }
-        console.log(lenta2.currentElementIndex);
-        lenta2.scrollTo({left: lenta2.children[lenta2.currentElementIndex].offsetLeft, behavior: 'smooth'});
-        if (lenta2.scrollWidth < lenta2.children[lenta2.currentElementIndex].offsetLeft + lenta2.offsetWidth) {
-            ring4.classList.add('disabledRing');
-        }
-    });
-    ring3.addEventListener('click', () => {
-        lenta2.currentElementIndex--;
-        if (lenta2.currentElementIndex <= 1) {
-            ring3.classList.add('disabledRing');
-        }
-        else {
-            ring3.classList.remove('disabledRing');
-            ring4.classList.remove('disabledRing');
-        }
-        if (lenta2.currentElementIndex <= 0) {
-            lenta2.currentElementIndex = 0;
-        }
-        console.log(lenta2.currentElementIndex);
-        lenta2.scrollTo({left: lenta2.children[lenta2.currentElementIndex].offsetLeft, behavior: 'smooth'});
-    });
 })
