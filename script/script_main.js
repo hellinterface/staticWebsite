342ffd3fbf56ebc370bf8c81ea65c2488ba39c55
Хабиба Икромова <ikromovahabiba28@gmail.com>
2023-06-03 11:36:47 +1000

    Gallery, animations

--- script/script_main.js
+++ script/script_main.js
@@ -9,5 +9,20 @@ document.addEventListener("DOMContentLoaded", function(){
     
     const mainContent = document.querySelector('.content');
 
+    function reveal() {
+        var reveals = document.querySelectorAll(".reveal");
+        for (var i = 0; i < reveals.length; i++) {
+          var windowHeight = window.innerHeight;
+          var elementTop = reveals[i].getBoundingClientRect().top;
+          var elementVisible = 150;
+          if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+          } else {
+            reveals[i].classList.remove("active");
+          }
+        }
+      }
 
+    window.addEventListener("scroll", () => reveal());
+    reveal();
 })
